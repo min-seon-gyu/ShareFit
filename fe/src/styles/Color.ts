5032a4d1a206de0748042b88ec09f1f2266cc41e
hoon <97011015+h00ns@users.noreply.github.com>
2024-07-02 21:55:05 +0900

    [FE] feat : setting & Login Layout (#1)
    * chore : yarn berry
    
    * chore : prettier
    
    * feat : Login Layout

---
+++ fe/src/styles/Color.ts
@@ -0,0 +1,45 @@
+export const primary = {
+  blue: "#5383E8",
+  gray: "#222222",
+} as const;
+
+// blue scale
+export const blue = {
+  blue1: "#EDF2FF",
+  blue2: "#C1D2FC",
+  blue3: "#4980F7",
+  blue4: "#0038B2",
+  blue5: "#01277A",
+} as const;
+
+// gray scale
+export const gray = {
+  gray1: "#F6F6F6",
+  gray2: "#EEEEEE",
+  gray3: "#E2E2E2",
+  gray4: "#CBCBCB",
+  gray5: "#AFAFAF",
+  gray6: "#808080",
+  gray7: "#535353",
+} as const;
+
+// red scale
+export const red = {
+  red1: "#FFCFCF",
+  red2: "#FF9B9B",
+  red3: "#FF5C5C",
+  red4: "#DF1D1D",
+  red5: "#BC0000",
+} as const;
+
+// green scale
+export const green = {
+  green1: "#D9F4D2",
+  green2: "#A6DA98",
+  green3: "#34963C",
+  green4: "#236F2A",
+  green5: "#113D15",
+} as const;
+
+export const white = "#FFFFFF" as const;
+export const black = "#000000" as const;
