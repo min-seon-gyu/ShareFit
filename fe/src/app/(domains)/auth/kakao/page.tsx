368ccd0b46dfa711a52390911b508a2b7207448c
h00ns <jihoon.hwang@imweb.me>
2024-07-09 22:14:13 +0900

    feat : 카카오로그인 1차 구현(리팩토링 필요)

--- fe/src/app/(domains)/auth/kakao/page.tsx
+++ fe/src/app/(domains)/auth/kakao/page.tsx
@@ -1,3 +1,57 @@
+'use client';
+
+import { KAKAO_API_KEY, KAKAO_REDIRECT_URI } from '@/constants/routes';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useRef } from 'react';
+
 export default function Kakao() {
-  return <div>kakao callback</div>;
+  const params = useSearchParams();
+
+  const code = params.get('code');
+
+  const isRequest = useRef(false);
+
+  useEffect(() => {
+    if (!code || isRequest.current) return;
+
+    isRequest.current = true;
+
+    const body = {
+      grant_type: 'authorization_code',
+      client_id: KAKAO_API_KEY,
+      redirect_uri: KAKAO_REDIRECT_URI,
+      code,
+    };
+
+    const getToken = async () => {
+      const response = await fetch('https://kauth.kakao.com/oauth/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+        body: new URLSearchParams(body).toString(),
+      }).then((res) => res.json());
+
+      const { access_token } = response;
+
+      await getInfo(access_token);
+    };
+
+    const getInfo = async (accessToken: string) => {
+      const response = await fetch('https://kapi.kakao.com/v2/user/me', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+      }).then((res) => res.json());
+
+      const { id } = response;
+      const { nickname } = response.kakao_account.profile;
+    };
+
+    getToken();
+  }, [code]);
+
+  return null;
 }
--- fe/src/constants/routes.ts
+++ fe/src/constants/routes.ts
@@ -1,4 +1,5 @@
-export const CLIENT_ID = '9b61abd2de726072ff00378114b97464';
-export const REDIRECT_URI = 'http://localhost:3000/auth/kakao';
+export const KAKAO_CLIENT_ID = '9b61abd2de726072ff00378114b97464';
+export const KAKAO_API_KEY = '09974a57055a1144c1de12eb21b30490';
+export const KAKAO_REDIRECT_URI = 'http://localhost:3000/auth/kakao';
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
