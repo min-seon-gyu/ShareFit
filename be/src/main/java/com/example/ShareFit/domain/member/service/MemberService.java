30dd79adb946dc882d1181fd02c7b7c616df44bd
Seongyu <gcael@naver.com>
2024-07-10 18:16:48 +0900

    MemberService getByUsername() 함수 삭제

--- be/src/main/java/com/example/ShareFit/domain/member/service/MemberService.java
+++ be/src/main/java/com/example/ShareFit/domain/member/service/MemberService.java
@@ -1,8 +1,6 @@
 package com.example.ShareFit.domain.member.service;
 
-import com.example.ShareFit.domain.member.Member;
 import com.example.ShareFit.domain.member.repository.MemberRepository;
-import com.example.ShareFit.security.auth.dto.AuthRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +10,4 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
-
-    public Member getByUsername(AuthRequestDto authRequestDto) {
-        Member member = memberRepository.findByUsername(authRequestDto.getUsername())
-                .orElseThrow(() -> new IllegalArgumentException("해당하는 멤버가 존재하지 않습니다."));
-        return member;
-    }
 }
