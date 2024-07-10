e7d39c09b6668c5f9127f217645db78d7e3c8c6d
Seongyu <gcael@naver.com>
2024-07-10 18:04:35 +0900

    엔티티 생성날짜, 최종수정날짜 데이터 추가

---
+++ be/src/main/java/com/example/ShareFit/common/JpaBaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.ShareFit.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class JpaBaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
