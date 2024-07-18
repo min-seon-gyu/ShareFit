67607f6979795909e5f0569cf5e902fb2ca90566
Seongyu <gcael@naver.com>
2024-07-18 21:56:27 +0900

    S3 서비스 및 설정파일 추가

---
+++ be/src/main/java/com/example/ShareFit/common/S3Service.java
@@ -0,0 +1,76 @@
+package com.example.ShareFit.common;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class S3Service {
+
+    private final AmazonS3 amazonS3;
+    private final String bucket;
+    private final static String DIR = "shareFit";
+
+    public S3Service(AmazonS3 amazonS3, @Value("${cloud.aws.s3.bucket}") String bucket) {
+        this.amazonS3 = amazonS3;
+        this.bucket = bucket;
+    }
+    public String upload(MultipartFile multipartFile) throws IOException {
+        String originalFileName = multipartFile.getOriginalFilename();
+
+        String uuid = UUID.randomUUID().toString();
+        String uniqueFileName = uuid + "_" + originalFileName.replaceAll("\\s", "_");
+
+        String fileName = DIR + "/" + uniqueFileName;
+        log.info("fileName: " + fileName);
+        File uploadFile = convert(multipartFile);
+
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+
+        String uploadImageUrl = amazonS3.getUrl(bucket, fileName).toString();
+        deleteLocalFile(uploadFile);
+        return uploadImageUrl;
+    }
+
+    private File convert(MultipartFile file) throws IOException {
+        String originalFileName = file.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+        String uniqueFileName = uuid + "_" + originalFileName.replaceAll("\\s", "_");
+
+        File convertFile = new File(uniqueFileName);
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            } catch (IOException e) {
+                log.error("파일 변환 중 오류 발생: {}", e.getMessage());
+                throw e;
+            }
+            return convertFile;
+        }
+        throw new IllegalArgumentException(String.format("파일 변환에 실패했습니다. %s", originalFileName));
+    }
+
+    private void deleteLocalFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+}
+
