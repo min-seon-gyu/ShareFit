8a3135478733c85e8c53b77b1fbeaf86545d5362
Seongyu <gcael@naver.com>
2024-07-18 22:44:39 +0900

    PostControllerDocs 리팩토링

--- be/src/main/java/com/example/ShareFit/common/swagger/PostControllerDocs.java
+++ be/src/main/java/com/example/ShareFit/common/swagger/PostControllerDocs.java
@@ -4,11 +4,12 @@ import com.example.ShareFit.domain.post.dto.PostCreateDto;
 import com.example.ShareFit.domain.post.dto.PostResponseDto;
 import com.example.ShareFit.domain.post.dto.PostUpdateDto;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
