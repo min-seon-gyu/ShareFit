b016febcebf0e9558486f40533ada362258f1036
h00ns <jihoon.hwang@imweb.me>
2024-07-19 16:52:35 +0900

    feat : 이미지 업로드, 게시물 등록

--- fe/src/apis/index.ts
+++ fe/src/apis/index.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
 const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -6,5 +6,27 @@ const API = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+/** Request */
+API.interceptors.request.use(
+  function (config: InternalAxiosRequestConfig) {
+    return config;
+  },
+
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+/** Response */
+API.interceptors.response.use(
+  async function (response: AxiosResponse) {
+    return response;
+  },
+
+  async function (error) {
+    return Promise.reject(error.response.data);
+  },
+);
 
 export default API;
