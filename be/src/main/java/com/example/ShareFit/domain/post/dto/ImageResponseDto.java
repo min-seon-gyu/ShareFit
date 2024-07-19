3bc2f1ab5b705d1168de0ae31b73ba1c1d75c54d
Seongyu <gcael@naver.com>
2024-07-19 15:43:21 +0900

    이미지 업로드 반환값 변경

---
+++ be/src/main/java/com/example/ShareFit/domain/post/dto/ImageResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.ShareFit.domain.post.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "이미지 등록 응답 DTO")
+public class ImageResponseDto {
+    @Schema(description = "이미지 경로")
+    private String imagePath;
+}
