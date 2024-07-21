f0bc28b0f1125f479bd25e8c3f56a38ff4cf49f8
Seongyu <gcael@naver.com>
2024-07-21 21:56:40 +0900

    [BE] sh 내용 변경

--- be/scripts/stop.sh
+++ be/scripts/stop.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-ROOT_PATH="/home/ubuntu/shareFit"
-JAR="$ROOT_PATH/application.jar"
-STOP_LOG="$ROOT_PATH/stop.log"
-SERVICE_PID=$(pgrep -f $JAR)
+PROJECT_ROOT="/home/ubuntu/shareFit"
+JAR_FILE="$PROJECT_ROOT/ShareFit-0.0.1-SNAPSHOT.jar"
 
-if [ -z $SERVICE_PID ]; then
-  echo "서비스 미실행" >> $STOP_LOG
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
-  echo "서비스 종료 " >> $STOP_LOG
-  kill -9 $SERVICE_PID
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
 fi
