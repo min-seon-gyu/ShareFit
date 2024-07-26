3c0ad42ad64776f9b9e6e705ebde1a1fbeb13237
SeonGyu <87053159+min-seon-gyu@users.noreply.github.com>
2024-07-26 17:29:39 +0900

    [BE] feat: Dto 생성 위치 변경, 게시글 반환 데이터에 댓글 수, 댓글 리스트 추가 (#19)

--- be/src/main/java/com/example/ShareFit/domain/like/Like.java
+++ be/src/main/java/com/example/ShareFit/domain/like/Like.java
@@ -18,11 +18,9 @@ public class Like extends JpaBaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "like_id")
     private Long id;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
--- be/src/main/java/com/example/ShareFit/domain/post/dto/PostDetailResponseDto.java
+++ be/src/main/java/com/example/ShareFit/domain/post/dto/PostDetailResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.ShareFit.domain.post.dto;
 
+import com.example.ShareFit.domain.comment.dto.CommentAllResponseDto;
 import com.example.ShareFit.domain.like.Like;
 import com.example.ShareFit.domain.post.Post;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -21,7 +22,7 @@ public class PostDetailResponseDto {
     @Schema(description = "게시글 이미지 경로")
     private String imagePath;
     @Schema(description = "게시글 좋아요 수")
-    private Long totalLike;
+    private Long totalLikeCount;
     @Schema(description = "게시글 좋아요 유무")
     private Boolean isLike;
     @Schema(description = "게시글 작성 회원 ID")
@@ -30,15 +31,19 @@ public class PostDetailResponseDto {
     private String nickname;
     @Schema(description = "게시글 작성 회원 프로필 경로")
     private String profilePath;
+    @Schema(description = "게시글 댓글")
+    private CommentAllResponseDto comments;
+
     public PostDetailResponseDto(Post post, Long userId){
         this.id = post.getId();
         this.content = post.getContent();
         this.imagePath = post.getImagePath();
-        this.totalLike = post.getTotalLike();
+        this.totalLikeCount = post.getTotalLikeCount();
         this.memberId = post.getMember().getId();
         this.nickname = post.getMember().getNickname();
         this.profilePath = post.getMember().getProfilePath();
         this.isLike = isLike(post, userId);
+        this.comments = new CommentAllResponseDto(post);
     }
 
     private boolean isLike(Post post, Long userId){
